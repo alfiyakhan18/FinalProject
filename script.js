c724e28972419868a413433c04fc81aa5faf0927
alfiyakhan18 <ialfiyakhan03@gmail.com>
2025-07-29 19:27:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function showWelcome() {
+  alert("Welcome to StylePoint! Start browsing your favourite styles.");
+}
+
+function validateOrder() {
+  alert("Your order has been placed successfully!");
+  return true; // Or you can do real validation if needed
+}
